1ecebb6dbc7d3261149d547c9586cccf03f0fa18
jacopo belli <jacopo.b2016@gmail.com>
2022-09-05 18:52:59 +0100

    typed.js

---
+++ app/javascript/controllers/auto_type_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from 'typed.js';
+
+export default class extends Controller {
+  connect () {
+    const typed = new Typed('.type', {
+    strings : ["Hi, my name is"],
+    typeSpeed: 130,
+    backSpeed: 50,
+    loop: true,
+    loopCount: 3
+    });
+};
+}
