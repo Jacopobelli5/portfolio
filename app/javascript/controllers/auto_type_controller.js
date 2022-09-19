222597a6a7f850bc0f8e38efee5a872a8c7bffa8
jacopo belli <jacopo.b2016@gmail.com>
2022-09-19 19:07:17 +0100

    about styling

--- app/javascript/controllers/auto_type_controller.js
+++ app/javascript/controllers/auto_type_controller.js
@@ -1,14 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
-import Typed from 'typed.js';
+import { Controller } from "@hotwired/stimulus";
+import Typed from "typed.js";
 
 export default class extends Controller {
-  connect () {
-    const typed = new Typed('.type', {
-    strings : ["Hi, my name is,"],
-    typeSpeed: 130,
-    backSpeed: 50,
-    loop: true,
-    loopCount: 5
+  connect() {
+    const typed = new Typed(".type", {
+      strings: ["Hi, my name is"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: true,
     });
-};
+  }
 }
